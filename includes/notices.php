d02200272c9486d6653a521c614255764d15e3ec
Andrew Munro <andrew@sumobi.com>
2017-03-05 12:01:27 +1300

    Add metabox with checkbox for sale notices

--- includes/notices.php
+++ includes/notices.php
@@ -67,17 +67,18 @@ function affwp_theme_display_notice() {
 
 								<?php echo do_shortcode( wpautop( $notice->post_content ) ); ?>
 
+								<?php if( ! get_post_meta($notice->ID, '_hide_close', true)) { ?>
+
+								<a class="remove-notice" href="#" id="remove-notice" rel="<?php echo $notice->ID; ?>">
+								<svg width="24px" height="24px">
+									   <use xlink:href="<?php echo get_stylesheet_directory_uri() . '/images/svgs/svg-defs.svg#icon-remove'; ?>"></use>
+									</svg>
+								</a>
+
+								<?php } ?>
+
 							</div>
 
-							<?php if( ! get_post_meta($notice->ID, '_hide_close', true)) { ?>
-
-							<a class="remove-notice" href="#" id="remove-notice" rel="<?php echo $notice->ID; ?>">
-							<svg width="24px" height="24px">
-								   <use xlink:href="<?php echo get_stylesheet_directory_uri() . '/images/svgs/svg-defs.svg#icon-remove'; ?>"></use>
-								</svg>
-							</a>
-
-							<?php } ?>
 					</div>
 				<?php }
 
@@ -132,3 +133,97 @@ function affwp_theme_discount_success() {
 		<?php
 }
 add_action( 'themedd_site_before', 'affwp_theme_discount_success' );
+
+/**
+ * Add a metabox to the notices post type
+ *
+ * @since  1.4.7
+ */
+function affwp_theme_notices_add_meta_box() {
+	add_meta_box( 'affwp_theme_alp_settings', 'Sale Notice', 'affwp_theme_notices_sale_meta_box', array( 'notices' ), 'side', 'default' );
+}
+add_action( 'add_meta_boxes', 'affwp_theme_notices_add_meta_box' );
+
+/**
+ * Add the meta box
+ *
+ * @since  1.4.7
+ */
+function affwp_theme_notices_sale_meta_box( $post ) {
+
+	$is_sale_notice = get_post_meta( $post->ID, 'affwp_notice_is_sale', true );
+
+	// Add an nonce field so we can check for it later.
+	wp_nonce_field( 'affwp_theme_sale_notice_nonce', 'affwp_theme_sale_notice_nonce' );
+
+?>
+	<p>	<input type="checkbox" name="affwp_notice_is_sale" id="affwp-notice-is-sale" value="1" <?php echo checked( $is_sale_notice, 1 ); ?>/>
+		<label for="affwp-notice-is-sale">This is a sale notice</label>
+	</p>
+
+<?php
+}
+
+/**
+ * Save post meta when the save_post action is called
+ *
+ * @since  1.4.7
+ * @param  int $post_id
+ * @global array $post All the data of the the current post
+ * @return void
+ */
+function affwp_theme_notices_save_meta_box( $post_id, $post ) {
+
+	/**
+	 * We need to verify this came from the our screen and with proper authorization,
+	 * because save_post can be triggered at other times.
+	 */
+
+	// Check if our nonce is set.
+	if ( ! isset( $_POST['affwp_theme_sale_notice_nonce'] ) ) {
+		return $post_id;
+	}
+
+	$nonce = $_POST['affwp_theme_sale_notice_nonce'];
+
+	// Verify that the nonce is valid.
+	if ( ! wp_verify_nonce( $nonce, 'affwp_theme_sale_notice_nonce' ) ) {
+		return $post_id;
+	}
+
+	// If this is an autosave, our form has not been submitted,
+	// so we don't want to do anything.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return $post_id;
+	}
+
+	// Check the user's permissions.
+	if ( 'notices' == $_POST['post_type'] ) {
+
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return $post_id;
+		}
+
+	} else {
+
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return $post_id;
+		}
+
+	}
+
+	// OK, its safe for us to save the data now.
+
+	$is_sale_notice = isset( $_POST['affwp_notice_is_sale'] ) ? sanitize_text_field( $_POST['affwp_notice_is_sale'] ) : '';
+
+	if ( $is_sale_notice ) {
+		// Update post meta.
+		update_post_meta( $post_id, 'affwp_notice_is_sale', true );
+	} else {
+		// Delete post meta.
+		delete_post_meta( $post_id, 'affwp_notice_is_sale' );
+	}
+
+}
+// Save metabox.
+add_action( 'save_post', 'affwp_theme_notices_save_meta_box', 10, 2 );
