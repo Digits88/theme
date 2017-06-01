1eb29b8c5b13cf33b6566c359de0d0ca1a89706d
Andrew Munro <andrew@sumobi.com>
2017-06-02 00:40:59 +1200

    footer tweaks

--- includes/footer.php
+++ includes/footer.php
@@ -11,14 +11,44 @@ function affwp_theme_remove_footer( $return ) {
 
 	return $return;
 }
-
 add_filter( 'themedd_footer_widgets_show', 'affwp_theme_remove_footer' );
 
+/**
+ * Set the number of widget areas to 3 (default is 4).
+ * This will only 3 sidebars on the admin widgets page.
+ */
+function affwp_theme_themedd_footer_widget_areas() {
+	return 3;
+}
+add_filter( 'themedd_footer_widget_areas', 'affwp_theme_themedd_footer_widget_areas' );
+
+/**
+ * We're only using 3 footer widgets in the admin but since we're dynamically appending a column (meet the family), change this to 4
+ */
+function affwp_theme_footer_widgets_columns_class() {
+	return 4;
+}
+add_filter( 'themedd_footer_widget_regions', 'affwp_theme_footer_widgets_columns_class' );
+
+/**
+ * Allows customization of the widget arguments
+ */
+function affwp_theme_footer_widget_tags( $widget_tags ) {
+
+	$widget_tags['before_title'] = '<h4 class="widget-title">';
+	$widget_tags['after_title']  = '</h4>';
+
+	return $widget_tags;
+
+}
+add_filter( 'themedd_footer_1_widget_tags', 'affwp_theme_footer_widget_tags' );
+add_filter( 'themedd_footer_2_widget_tags', 'affwp_theme_footer_widget_tags' );
+add_filter( 'themedd_footer_3_widget_tags', 'affwp_theme_footer_widget_tags' );
 
 function affwp_theme_add_footer_column() {
 	?>
 
-	<div class="widget-column meet-the-family">
+	<div class="footer-widget widget-column meet-the-family col-xs-12 col-sm-6 col-lg-3">
 
 		<h4>Meet the family</h4>
 		<ul>
@@ -43,21 +73,6 @@ function affwp_theme_add_footer_column() {
 	<?php
 }
 add_action( 'themedd_footer_widgets_end', 'affwp_theme_add_footer_column' );
-
-/**
- * Allows customization of the widget arguments
- */
-function affwp_theme_footer_widget_tags( $widget_tags ) {
-
-	$widget_tags['before_title'] = '<h4 class="widget-title">';
-	$widget_tags['after_title']  = '</h4>';
-
-	return $widget_tags;
-
-}
-add_filter( 'themedd_footer_1_widget_tags', 'affwp_theme_footer_widget_tags' );
-add_filter( 'themedd_footer_2_widget_tags', 'affwp_theme_footer_widget_tags' );
-add_filter( 'themedd_footer_3_widget_tags', 'affwp_theme_footer_widget_tags' );
 
 /**
  * Mascot
